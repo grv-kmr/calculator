4a81b9ce56f7ca6861dacca1b846490f2f3cc7a9
Gaurav Kumar <grvkmr2007@gmail.com>
2024-09-16 13:36:00 +0530

    Add files via upload

---
+++ sim_calc.js
@@ -0,0 +1,17 @@
+output = document.getElementById("output");
+function disp_num(input) {
+    output.value += input;  
+}
+function rem_num() {
+  output.value = output.value.slice(0, output.value.length - 1);
+}
+function clear_disp() {
+  output.value = "";
+}
+function final() {
+  if (output.value != "") {
+    output.value = eval(output.value);
+  } else {
+    output.value = "write something first";
+  }
+}
